7fa235aa4f48c1ef5eede7e25ccd3b78acfc7001
L0rdKunz <90391791+L0rdKunz@users.noreply.github.com>
2022-03-29 16:52:20 -0300

    Update esp32.cpp

--- central-de-monitoramento/esp32.cpp
+++ central-de-monitoramento/esp32.cpp
@@ -1,62 +1,50 @@
 #include "DHT.h"
-#include "PubSubClient.h" // Connect and publish to the MQTT broker
+#include "PubSubClient.h" 
 
-// Code for the ESP32
-#include "WiFi.h" // Enables the ESP32 to connect to the local network (via WiFi)
-#define DHTPIN 4  // Pin connected to the DHT sensor
+#define DHTPIN 4 
 
-// Code for the ESP8266
-//#include "ESP8266WiFi.h"  // Enables the ESP8266 to connect to the local network (via WiFi)
-//#define DHTPIN D5         // Pin connected to the DHT sensor
-
-#define DHTTYPE DHT22  // DHT11 or DHT22
+#define DHTTYPE DHT22 
 DHT dht(DHTPIN, DHTTYPE);
 
 // WiFi
-const char* ssid = "TP_LINK-804";                 // Your personal network SSID
-const char* wifi_password = "Xuv@10?z"; // Your personal network password
+const char* ssid = "TP_LINK-804";                 
+const char* wifi_password = "senha_do_wifi"
 
 // MQTT
-const char* mqtt_server = "192.168.0.106";  // IP of the MQTT broker
+const char* mqtt_server = "192.168.0.106";  // IP MQTT server
 const char* humidity_topic = "Humidade: ";
 const char* temperature_topic = "Temperatura: ";
 const char* mqtt_username = "erick"; // MQTT username
 const char* mqtt_password = "123"; // MQTT password
 const char* clientID = "client_livingroom"; // MQTT client ID
 
-// Initialise the WiFi and MQTT Client objects
+// Inicializa WiFi e MQTT Client
 WiFiClient wifiClient;
-// 1883 is the listener port for the Broker
+
+// escuta pela porta 1883
 PubSubClient client(mqtt_server, 1883, wifiClient);
 
-
-// Custom function to connet to the MQTT broker via WiFi
 void connect_MQTT(){
   Serial.print("Connecting to ");
   Serial.println(ssid);
 
-  // Connect to the WiFi
+  // Conectado no WiFi
   WiFi.begin(ssid, wifi_password);
 
-  // Wait until the connection has been confirmed before continuing
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
-
-  // Debugging - Output the IP Address of the ESP8266
-  Serial.println("WiFi connected");
+ 
+  Serial.println("WiFi conectado");
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
 
-  // Connect to MQTT Broker
-  // client.connect returns a boolean value to let us know if the connection was successful.
-  // If the connection is failing, make sure you are using the correct MQTT Username and Password (Setup Earlier in the Instructable)
   if (client.connect(clientID, mqtt_username, mqtt_password)) {
-    Serial.println("Connected to MQTT Broker!");
+    Serial.println("Connectado ao MQTT!");
   }
   else {
-    Serial.println("Connection to MQTT Broker failed...");
+    Serial.println("Falha ao conectar no MQTT...");
   }
 }
 
@@ -73,42 +61,37 @@ void loop() {
   float h = dht.readHumidity();
   float t = dht.readTemperature();
 
-  Serial.print("Humidity: ");
+  Serial.print("Umidade: ");
   Serial.print(h);
   Serial.println(" %");
-  Serial.print("Temperature: ");
+  Serial.print("Temperatura: ");
   Serial.print(t);
   Serial.println(" *C");
 
-  // MQTT can only transmit strings
-  String hs="Hum: "+String((float)h)+" % ";
-  String ts="Temp: "+String((float)t)+" C ";
+  String hs="Umidade: "+String((float)h)+" % ";
+  String ts="Temperatura: "+String((float)t)+" C ";
 
-  // PUBLISH to the MQTT Broker (topic = Temperature, defined at the beginning)
   if (client.publish(temperature_topic, String(t).c_str())) {
-    Serial.println("Temperature sent!");
+    Serial.println("Temperatura enviada!");
   }
-  // Again, client.publish will return a boolean value depending on whether it succeded or not.
-  // If the message failed to send, we will try again, as the connection may have broken.
   else {
-    Serial.println("Temperature failed to send. Reconnecting to MQTT Broker and trying again");
+    Serial.println("Falha ao enviar Temperatura. Reconectando ao MQTT Broker e tentando novamente");
     client.connect(clientID, mqtt_username, mqtt_password);
-    delay(10); // This delay ensures that client.publish doesn't clash with the client.connect call
+    delay(10); 
     client.publish(temperature_topic, String(t).c_str());
   }
 
-  // PUBLISH to the MQTT Broker (topic = Humidity, defined at the beginning)
+  
   if (client.publish(humidity_topic, String(h).c_str())) {
-    Serial.println("Humidity sent!");
+    Serial.println("Umidade enviada!");
   }
-  // Again, client.publish will return a boolean value depending on whether it succeded or not.
-  // If the message failed to send, we will try again, as the connection may have broken.
+  
   else {
-    Serial.println("Humidity failed to send. Reconnecting to MQTT Broker and trying again");
+    Serial.println("Falha ao enviar Humidade. Reconectando ao MQTT Broker e tentando novamente");
     client.connect(clientID, mqtt_username, mqtt_password);
-    delay(10); // This delay ensures that client.publish doesn't clash with the client.connect call
+    delay(10); 
     client.publish(humidity_topic, String(h).c_str());
   }
-  client.disconnect();  // disconnect from the MQTT broker
-  delay(100);       // print new values every 1 Minute
+  client.disconnect(); 
+  delay(100);  
 }
